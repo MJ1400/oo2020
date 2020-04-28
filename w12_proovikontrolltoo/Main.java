3e4d4bd93fddbf53f7419c6a6ce50f32405a206e
raimop <ray@tlu.ee>
2020-04-28 09:23:38 +0300

    Generics extra

--- w12_proovikontrolltoo/Main.java
+++ w12_proovikontrolltoo/Main.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,12 +11,14 @@ public class Main {
         List<Integer> integers = Arrays.asList(1, 2, 3);
         List<Character> characters = Arrays.asList('a', 'b', 'c');
         List<Double> doubles = Arrays.asList(1.0, 2.0, 3.0);
+        List<String> strings2 = new ArrayList<>();
 
         System.out.println(
             Generics.getFirst(strings) + " " +
             Generics.getFirst(integers) + " " + 
             Generics.getFirst(characters) + " " + 
-            Generics.getFirst(doubles)
+            Generics.getFirst(doubles) + " " +
+            Generics.getFirst(strings2)
         );
 
         SimplePair<String, String> pair = new SimplePair<>("name", "Raimo");
@@ -21,6 +26,14 @@ public class Main {
         System.out.println(
             pair.getKey() + " " + 
             pair.getValue()
+        );
+
+        SimplePair<String, SimplePair<String, String>> pair2 = new SimplePair<>("data", new SimplePair<>("info", "Raimo"));
+        // SimplePair<String, SimplePair<String, SimplePair<String, String>>> pair3 = new SimplePair<>("data", new SimplePair<>("info", new SimplePair<>("info2", "Raimo" )));
+
+        System.out.println(
+            pair2.getKey() + " \n\t" + 
+            pair2.getValue().getKey() + ": " + pair2.getValue().getValue()
         );
 
         Car car = new Car();
@@ -32,6 +45,21 @@ public class Main {
             Generics.getWheels(car) + " " +
             Generics.getWheels(car2) + " " +
             Generics.getWheels(vehicle)
+        );
+
+        // Generics.getType
+
+        Map<String, String> map = new HashMap<>();
+
+        System.out.println(
+            Generics.getType(1) + " " +
+            Generics.getType("Raimo") + " " + 
+            Generics.getType(1.0) + " " + 
+            Generics.getType((byte) 5) + " " +
+            Generics.getType((long) 5) + " " +
+            Generics.getType(map) + " " +
+            Generics.getType(pair2) + " " +
+            Generics.getType(strings)
         );
 
     }
